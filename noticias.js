37180fcfb04f1006bcf805773f6e9a77f36bfa1b
wendelspires <wendelspires@hotmail.com>
2020-07-09 11:41:48 -0500

    Implementando as rotas

--- noticias.js
+++ noticias.js
@@ -2,7 +2,28 @@ var http = require ('http');
 
 var server = http.createServer(function (req, res){
 
-  res.end("<html><body>Track Record de Estrategias</body></html>")
+  var categoria = req.url;
+
+  if(categoria == '/tecnologia'){
+    res.end("<html><body>Noticias de tecnologia</body></html>")
+  }
+
+  else if(categoria == '/mercado'){
+    res.end("<html><body>Noticias de mercado</body></html>")
+  }
+
+  else if(categoria == '/robôs'){
+    res.end("<html><body>Noticias de mercado</body></html>")
+  }
+
+  else if(categoria == '/cnpi'){
+    res.end("<html><body>Melhor analista CNPI do Brasil!!</body></html>")
+  }
+
+
+  else {
+    res.end("<html><body>Track Record de Estrategias</body></html>")
+  }
 
 });
 
