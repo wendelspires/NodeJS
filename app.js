8f694018810302cc02c3030c791abb4e6b38c58d
wendelspires <wendelspires@hotmail.com>
2020-07-10 11:19:46 -0500

    Importanto todos os arquivos para o Git

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 var express = require('express');
+var msg = require('./mod_teste')();
+
 var app = express();
 
 app.set('view engine', 'ejs');
@@ -17,5 +19,5 @@ app.get('/noticias', function(req,res){
 });
 
 app.listen(3000, function(){
-  console.log("Servidor rodando com o Express");
+  console.log(msg);
 });
