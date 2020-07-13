3b5d8b706a44821d08932cd33a40b3b9a2d172ee
wendelspires <wendelspires@hotmail.com>
2020-07-13 16:02:03 -0500

    Integrando o banco de dados com o servidor

--- app/routes/noticias.js
+++ app/routes/noticias.js
@@ -1,20 +1,12 @@
+var dbConnection = require('../../config/dbConnection');
+
 module.exports = function(app) {
 
-  app.get('/noticias', function(req,res){
+    var connection = dbConnection();
 
-    var mysql = require('mysql');
-
-    var connection = mysql.createConnection({
-        host : 'localhost',
-        user : 'root',
-        password: 'root',
-        database : 'portal_noticias'
-      });
-
-    connection.query('select * from noticias',function(error, result){
-      res.send(result);
+    app.get('/noticias', function(req,res){
+        connection.query('select * from noticias',function(error, result){
+          res.render("noticias/noticias", {noticias : result});
     });
-
-    //res.render("noticias/noticias")
   });
 };
