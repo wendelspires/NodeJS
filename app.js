99544c1c46d0b540c34fc55cae02b8aabed7f930
wendelspires <wendelspires@hotmail.com>
2020-07-13 09:46:18 -0500

    instalando o mysql

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 var app = require('./config/server');
 
-var rota_noticias = require('./app/routes/noticias');
-rota_noticias(app);
+var rota_noticias = require('./app/routes/noticias')(app);
 
-var rota_home = require ('./app/routes/home');
-rota_home(app);
+var rota_home = require ('./app/routes/home')(app);
 
-var rota_formulario_inclusao_noticias = require('./app/routes/formulario_inclusao_noticia')
-rota_formulario_inclusao_noticias(app);
-
+var rota_formulario_inclusao_noticias = require('./app/routes/formulario_inclusao_noticia')(app);
 
 app.listen(3000, function(){
   console.log('Servidor ON');
