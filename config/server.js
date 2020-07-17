42b2a7b9d29cc3803d4b9e7eae6d0faba74cd60c
wendelspires <wendelspires@hotmail.com>
2020-07-17 10:51:06 -0500

    Instalando o Express validator

--- config/server.js
+++ config/server.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var consign = require('consign');
 var bodyparser = require('body-parser');
-
+var  {check, expressValidator} = require('express-validator');
 
 var app = express();
 app.set('view engine', 'ejs');
 app.set('views','./app/views');
 
 app.use(bodyparser.urlencoded({extended: true}));
-
+app.use(check);
 
 //consign para carregar todos os módulos ao inicializar a aplicação.
 consign()
